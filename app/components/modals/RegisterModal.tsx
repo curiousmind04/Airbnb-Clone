383bf8f2561a0e5c3228d1d57ca0debb80f426c4
Brandon Bhangari <b6_10_94_furlonge@hotmail.com>
2023-06-29 12:01:58 -0400

    created our models with prisma

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -89,7 +89,10 @@ const RegisterModal = () => {
       <div className=" text-neutral-500 text-center mt-4 font-light">
         <div className="justify-center flex flex-row items-center gap-2">
           <div>Already have an account?</div>
-          <div className="text-neutral-800 cursor-pointer hover:underline">
+          <div
+            onClick={registerModal.onClose}
+            className="text-neutral-800 cursor-pointer hover:underline"
+          >
             Log in
           </div>
         </div>
