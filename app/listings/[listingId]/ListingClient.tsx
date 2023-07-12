db157c4be48836b8648eec0c22d6149139332b5d
Brandon Bhangari <b6_10_94_furlonge@hotmail.com>
2023-07-11 23:21:55 -0400

    created SafeListing type, created getListingById, started working on page for individual listings

---
+++ app/listings/[listingId]/ListingClient.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { SafeListing, SafeUser } from "@/app/types";
+import { Reservation } from "@prisma/client";
+
+interface ListingClientProps {
+  reservations?: Reservation[];
+  listing: SafeListing & {
+    user: SafeUser;
+  };
+  currentUser?: SafeUser | null;
+}
+
+const ListingClient: React.FC<ListingClientProps> = ({
+  listing,
+  currentUser,
+}) => {
+  return <></>;
+};
+export default ListingClient;
